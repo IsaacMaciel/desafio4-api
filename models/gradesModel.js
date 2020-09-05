93facc36fa7b8f554b12183b7fdbdb9e194552ff
Isaac Araujo <isaac_maciel@hotmail.com>
2020-09-05 09:29:06 -0300

    modificando o toJSON

--- models/gradesModel.js
+++ models/gradesModel.js
@@ -22,7 +22,7 @@ export default (mongoose) => {
     },
   });
 
-  schema.method("toJSON", () => {
+  schema.method('toJSON', function () {
     const { __v, _id, ...object } = this.toObject();
 
     object.id = _id;
